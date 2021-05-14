c51b142871355d299664a724a971b82643e1c141
danro9685 <daniele.ramazzotti@yahoo.com>
2021-05-14 18:37:16 +0200

    import data

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# VirMutSig
+VirMutSig
+================
--- step1_data_preprocessing/AMPLICON/run.sh
+++ step1_data_preprocessing/AMPLICON/run.sh
@@ -1,3 +1,4 @@
-nohup bash 1_make_fastq_paired_end.sh > 1_make_fastq_paired_end.log
-nohup bash 2_preprocessing_paired_end.sh > 2_preprocessing_paired_end.log
-nohup bash 3_variant_calling.sh > 3_variant_calling.log
+bash 1_make_fastq_paired_end.sh
+bash 2_preprocessing_paired_end.sh
+bash 3_variant_calling.sh
+Rscript 4_import_data.R
